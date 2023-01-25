79582391d5c3ed1799b00982b00ad2c1e4e5fa62
ItaySagy <itaysagy15999@gmail.com>
2023-01-25 15:28:18 +0200

    Initial commit

---
+++ client.py
@@ -0,0 +1,76 @@
+import pygame
+import pygame.display
+from network import Network
+
+width = 500
+height = 500
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+clientNumber = 0
+
+
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x, y, width, height)
+        self.vel = 3
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
+
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
+
+        if keys[pygame.K_UP]:
+            self.y -= self.vel
+
+        if keys[pygame.K_DOWN]:
+            self.y += self.vel
+        self.rect = (self.x, self.y, self.width, self.height)
+
+
+def read_pos(srt):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+def redrawWindow(win, Player):
+    win.fill((255, 255, 255))
+    Player.draw(win)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    n = Network()
+    startPos = read_pos(n.getPos())
+    p1 = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0))
+    p2 = Player(0, 0, 100, 100, (0, 255, 0))
+    clock = pygame.time.Clock()
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+        p1.move()
+        redrawWindow(win, p1)
+        
+
+
+main()
